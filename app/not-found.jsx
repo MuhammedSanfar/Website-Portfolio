d8ea6c71d7746fad70a7e4cabf468430caab11e5
MuhammedSanfar <muhammed.sanfar@nuventure.in>
2025-02-20 14:36:10 +0530

    Fifth Commit

--- app/not-found.jsx
+++ app/not-found.jsx
@@ -11,7 +11,7 @@ export default function NotFound() {
   return (
     <Center className='h-screen'>
       <div className='select-none'>
-        <h1 className='text-[max(9.5em,16vw)]'>Not Found</h1>
+        <h1 className='text-[max(9.5em,16vw)]'>Not Found.</h1>
       </div>
     </Center>
   );
